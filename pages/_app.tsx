25fc48ac49f99c14caee733487bf69f315492af4
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-10-03 10:28:25 +0200

    Thank you Create Cosmos App!

---
+++ pages/_app.tsx
@@ -0,0 +1,44 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { WalletProvider } from '@cosmos-kit/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { defaultTheme } from '../config';
+import { wallets } from '@cosmos-kit/keplr';
+import { assets, chains } from 'chain-registry';
+import { getSigningCosmosClientOptions } from 'osmojs';
+import { GasPrice } from '@cosmjs/stargate';
+
+import { SignerOptions } from '@cosmos-kit/core';
+import { Chain } from '@chain-registry/types';
+
+function CreateCosmosApp({ Component, pageProps }: AppProps) {
+  const signerOptions: SignerOptions = {
+    stargate: (_chain: Chain) => {
+      return getSigningCosmosClientOptions();
+    },
+    cosmwasm: (chain: Chain) => {
+      switch (chain.chain_name) {
+        case 'osmosis':
+        case 'osmosistestnet':
+          return {
+            gasPrice: GasPrice.fromString('0.0025uosmo')
+          };
+      }
+    }
+  };
+
+  return (
+    <ChakraProvider theme={defaultTheme}>
+      <WalletProvider
+        chains={chains}
+        assetLists={assets}
+        wallets={wallets}
+        signerOptions={signerOptions}
+      >
+        <Component {...pageProps} />
+      </WalletProvider>
+    </ChakraProvider>
+  );
+}
+
+export default CreateCosmosApp;
