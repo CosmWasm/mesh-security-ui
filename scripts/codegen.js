9c2b2fa0ee6a62d4f42dac6971a88de81a02511b
Josef Leventon <josef.leventon@gmail.com>
2022-10-05 18:27:04 -0500

    Add message-composer to codegen

--- scripts/codegen.js
+++ scripts/codegen.js
@@ -1,56 +1,58 @@
-const { join, resolve }  = require('path');
-const codegen = require('@cosmwasm/ts-codegen').default;
+const { join, resolve } = require("path");
+const codegen = require("@cosmwasm/ts-codegen").default;
 
-const contractsDir = resolve(join(__dirname, '/../schemas'));
+const contractsDir = resolve(join(__dirname, "/../schemas"));
 const contracts = [
   {
-    name: 'HackCw20',
-    dir: join(contractsDir, 'cw20-base')
+    name: "HackCw20",
+    dir: join(contractsDir, "cw20-base"),
   },
   {
-    name: 'MeshConsumer',
-    dir: join(contractsDir, 'mesh-consumer')
+    name: "MeshConsumer",
+    dir: join(contractsDir, "mesh-consumer"),
   },
   {
-    name: 'MeshLockup',
-    dir: join(contractsDir, 'mesh-lockup')
+    name: "MeshLockup",
+    dir: join(contractsDir, "mesh-lockup"),
   },
   {
-    name: 'MeshProvider',
-    dir: join(contractsDir, 'mesh-provider')
+    name: "MeshProvider",
+    dir: join(contractsDir, "mesh-provider"),
   },
   {
-    name: 'MeshSlasher',
-    dir: join(contractsDir, 'mesh-slasher')
+    name: "MeshSlasher",
+    dir: join(contractsDir, "mesh-slasher"),
   },
   {
-    name: 'MetaStaking',
-    dir: join(contractsDir, 'meta-staking')
+    name: "MetaStaking",
+    dir: join(contractsDir, "meta-staking"),
   },
 ];
 
 codegen({
   contracts,
-  outPath: join(__dirname, '../codegen'),
+  outPath: join(__dirname, "../codegen"),
   options: {
     bundle: {
       enabled: true,
-      bundleFile: 'index.ts',
-      scope: 'contracts'
+      bundleFile: "index.ts",
+      scope: "contracts",
     },
     types: {
-      enabled: true
+      enabled: true,
     },
     client: {
-      enabled: true
+      enabled: true,
     },
     messageComposer: {
-      enabled: false
-    }
-  }
-}).then(() => {
-  console.log('✨ all done!');
-}).catch(e=>{
-  console.error(e);
-  process.exit(1)
-});
+      enabled: true,
+    },
+  },
+})
+  .then(() => {
+    console.log("✨ all done!");
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
