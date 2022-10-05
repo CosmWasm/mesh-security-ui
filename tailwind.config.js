fd061ab7e6e5c279dfa9bf84f9de039ef0a44a04
Josef Leventon <josef.leventon@gmail.com>
2022-10-05 18:26:40 -0500

    Config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,26 @@ module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
+    "./hooks/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#6674D9",
+          50: "#C2C8F0",
+          100: "#B8BEED",
+          200: "#A3ACE8",
+          300: "#8F99E3",
+          400: "#7A87DE",
+          500: "#6674D9",
+          600: "#4D5ED3",
+          700: "#3547CD",
+          800: "#2D3EB6",
+          900: "#27369D",
+        },
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
